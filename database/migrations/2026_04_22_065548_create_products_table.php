c4aa3b430ef72ffa3cc82f3cd9784f3bd5d99f35
7Demon <dickyramadhan543@gmail.com>
2026-04-29 08:59:15 +0700

    feat: add atribute table product> size column

--- database/migrations/2026_04_22_065548_create_products_table.php
+++ database/migrations/2026_04_22_065548_create_products_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description');
+            $table->string('Size');
             $table->integer('stock');
             $table->decimal('price', 8, 2);
             $table->integer('reserved_stock')->default(0);
